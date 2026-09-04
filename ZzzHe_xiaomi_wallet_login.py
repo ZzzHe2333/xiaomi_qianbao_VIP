a94051cada592643a827aa168d822a7af2cf2b3c
ZzzHe <x121144@163.com>
2026-09-04 21:36:01 +0800

    Add startup banner to login task

--- ZzzHe_xiaomi_wallet_login.py
+++ ZzzHe_xiaomi_wallet_login.py
@@ -5,8 +5,10 @@ name: ZzzHe_小米钱包扫码登录
 cron: 0 0 29 2 *
 """
 
+from xiaomi_common import print_project_banner
 from xiaomi_login import main
 
 
 if __name__ == "__main__":
+    print_project_banner("ZzzHe_小米钱包扫码登录")
     raise SystemExit(main())
